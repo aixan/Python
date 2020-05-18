459e9b6beeffbdb577d5529c63046604a7c75c64
aixan <admin@aixan.cn>
2020-05-19 01:10:07 +0800

    优化代码

--- TCP-server.py
+++ TCP-server.py
@@ -14,6 +14,7 @@ PORT = 3000  # 端口号
 BS = 8192  # 接收数据缓冲大小
 ADDR = (HOST, PORT)
 glnr = "zabbix"
+COD = 'utf-8'
 
 tcpSerSock = socket(AF_INET, SOCK_STREAM)  # 创建TCP服务器套接字
 tcpSerSock.setsockopt(SOL_SOCKET, SO_REUSEADDR, 1)
@@ -31,10 +32,10 @@ while True:
             try:
                 data = tcpCliSock.recv(BS)  # 连续接收指定字节的数据，接收到的是字节>数组
                 if data:  # 如果数据空白，则表示客户端退出，所以退出接收
-                    print("客户端发送的内容：",data.decode('utf-8'))
+                    print("客户端发送的内容：",data.decode(COD))
                     date = time.strftime('%Y-%m-%d %H:%M:%S', time.localtime(time.time()))
-                    if glnr.encode('utf8') in data:
-                        dd = "./dingding.py {}".format(data.decode(encoding='UTF-8'), 'ignore')
+                    if glnr.encode(COD) in data:
+                        dd = "./dingding.py {}".format(data.decode(encoding='UTF-8'), errors='ignore')
                         p = subprocess.call(dd, shell=True)
                         if p == 0:
                             time.sleep(4)
@@ -45,7 +46,7 @@ while True:
                     else:
                         print("忽略发送：{}".format(date))
                     msg = "YES"
-                    tcpCliSock.send(msg.encode('utf8'))  # 向客户端状态数据
+                    tcpCliSock.send(msg.encode(COD))  # 向客户端状态数据
                     print("=" * 30)
                 else:
                     break
